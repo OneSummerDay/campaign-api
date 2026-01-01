651d706dfb8d5cc1a15b98be2bf69da981691313
OneSummerDay <artem.kraievyi@gmail.com>
2026-01-01 18:21:20 +0200

    feat: add mock campaigns data

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
+from datetime import datetime
 from fastapi import FastAPI
 
 
 app = FastAPI(root_path="/api/v1")
 
+data = [
+    {
+        "campaing_id": 1,
+        "name": "Campaign 1",
+        "due_date": datetime.now(),
+        "created_at": datetime.now(),
+    },
+    {
+        "campaing_id": 2,
+        "name": "Campaign 2",
+        "due_date": datetime.now(),
+        "created_at": datetime.now(),
+    },
+]
+
+
 @app.get("/campaigns")
 async def read_campaigns():
-    return {"message": "List of campaigns"}
+    return {"message": data}
 
 
 
